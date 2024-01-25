6e94833d4cd22e5a4710a002f6a6b5f0c14faac4
Jordan Pinnick <geometryse@gmail.com>
2024-01-24 19:36:16 -0500

    Initialize Tauri API

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,11 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+#[tauri::command]
+fn my_custom_command(name: &str) {
+  println!("I was invoked from JS! hello, {}.", name);
 }
