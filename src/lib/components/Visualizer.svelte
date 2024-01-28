38a73ed9679b5aa01c9e6c993224a4475e47e569
Jordan Pinnick <geometryse@gmail.com>
2024-01-27 20:28:07 -0500

    Center Visualizer more

--- src/lib/components/Visualizer.svelte
+++ src/lib/components/Visualizer.svelte
@@ -86,7 +86,7 @@
 
 {#if visible}
 	<div
-		class="w-full max-w-7xl h-[500px] flex items-center justify-center gap-[2px]"
+		class="w-full max-w-7xl h-[500px] flex items-center justify-center gap-[2px] mr-12"
 		transition:fade|global
 	>
 		{#each heights as dot, i (i)}
