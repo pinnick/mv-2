1011ca1d35558e92de36ccbd5f121ecbdf19a707
Jordan Pinnick <geometryse@gmail.com>
2024-02-01 16:17:11 -0500

    Add explicit (again)

--- src/lib/components/Player/TitleArtist/TitleArtist.svelte
+++ src/lib/components/Player/TitleArtist/TitleArtist.svelte
@@ -5,21 +5,36 @@
 	$: title = $metadata?.title || ($mediaElement ? 'Unnamed track' : 'Not Playing');
 	$: album = $metadata?.album || '';
 	$: artist = $metadata?.artist || '';
-	$: explicit = !!$metadata?.explicit;
+	// $: explicit = !!$metadata?.explicit;
 	$: bottomText = artist + (showAlbum && album ? ` — ${album}` : '');
-	$: console.log($metadata?.color);
 	let showAlbum = false;
+
+	let explicit = false;
+	const path =
+		'M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 6h-3v2h3c.55 0 1 .45 1 1s-.45 1-1 1h-3v2h3c.55 0 1 .45 1 1s-.45 1-1 1h-4c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1h4c.55 0 1 .45 1 1s-.45 1-1 1z';
+
+	const toggleExplicit = () => {
+		explicit = !explicit;
+	};
 </script>
 
 <div class="w-full text-[22px] flex-1 min-w-0">
 	<div class="font-semibold -mb-1.5 pr-1 text-white">
-		<Marquee>{title}</Marquee>
+		<Marquee
+			><button on:click={toggleExplicit} class="flex items-center justify-center"
+				>{title}
+				{#if explicit}
+					<span class="ml-2">
+						<svg width={18} height={18} viewBox="3 3 18 18" class="explicit-icon"
+							><path fill="color-mix(in srgb, {$metadata?.color || '#fff'}, #ccc)" d={path} /></svg
+						>
+					</span>
+				{/if}
+			</button>
+		</Marquee>
 	</div>
 	{#if bottomText.length > 0}
-		<div
-			class="text-[22px]"
-			style="color: color-mix(in srgb, {$metadata?.color || '#ffffff'}, #ffffff)"
-		>
+		<div class="text-[22px]" style="color: color-mix(in srgb, {$metadata?.color || '#fff'}, #fff)">
 			<Marquee opacity={0.8}>{artist}</Marquee>
 		</div>
 	{:else}
