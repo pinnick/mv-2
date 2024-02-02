04c9f9a93a2edf4c4ff1e59ab87b729c17bb2833
Jordan Pinnick <geometryse@gmail.com>
2024-02-01 19:58:52 -0500

    Fix gap for marquee

--- src/lib/components/Player/TitleArtist/Marquee2.svelte
+++ src/lib/components/Player/TitleArtist/Marquee2.svelte
@@ -3,14 +3,15 @@
 	let parentWidth: number;
 	let childWidth: number;
 
-	$: overflow = childWidth >= parentWidth;
+	// Minus 50 to start fade slightly before it actually overflows (padding)
+	$: overflow = childWidth >= parentWidth - 50;
 	// A maskRight value of "100%" will eliminate the right-side gradient, and "92%" will make it visible.
 	$: maskRight = overflow ? '92%' : '100%';
 </script>
 
 <div class="whitespace-nowrap w-[110%] -ml-[10%] marquee" bind:clientWidth={parentWidth}>
 	<p
-		class="w-min max-w-full marquee-text pl-[10%] flex"
+		class="w-min max-w-full marquee-text pl-[10%] flex gap-10"
 		style="--opacity: {opacity}; --maskRight: {maskRight}"
 	>
 		<span bind:clientWidth={childWidth}><slot /></span>
